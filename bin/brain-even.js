2fdd12d5435dfa4c79394c926639c2e1b4365f42
Svetlana <pidgin66@gmail.com>
2023-02-09 15:23:35 +0300

    fix acting file

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,41 +1,5 @@
 #!/usr/bin/env node
 
-import _ from 'lodash';
-import readlineSync from 'readline-sync';
-import {welcomeUser, userName} from './src/cli.js';
+import brainEven from '../src/games/even_game.js'
 
-console.log(welcomeUser());
-const brainEven =  () => {
-    for (let i = 0; i < 3; i += 1) {
-
-        let randomNumber = _.random(0, 100);
-
-        console.log(`Question: ${randomNumber}`);
-
-        const userAnswer = readlineSync.question('Your answer: ');
-
-        if (randomNumber % 2 === 0 && userAnswer === 'yes') {
-        console.log('Correct!');
-        } else if (randomNumber % 2!== 0 && userAnswer === 'no') {
-        console.log('Correct!');
-        } else if (randomNumber % 2 === 0 && userAnswer === 'no') {
-        console.log (`${userAnswer} is wrong answer ;(. Correct answer was 'yes'.`);
-        console.log(`Let's try again, ${userName}!`);
-        break;
-        } else if (randomNumber % 2!== 0 && userAnswer === 'yes') {
-        console.log (`${userAnswer} is wrong answer ;(. Correct answer was 'no'.`);
-        console.log(`Let's try again, ${userName}!`);
-        break;
-        } else {
-            console.log(`Let's try again, ${userName}!`)
-            break;
-        }
-        console.log(`Congratulations, ${userName}!`);
-    }
-
-    
-};
-
-console.log(brainEven());
-
-export default brainEven;
+brainEven();
