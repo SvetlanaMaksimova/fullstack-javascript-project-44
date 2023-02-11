6d18f96ff76984e77a4a0b50be15d527f835d12a
Svetlana <pidgin66@gmail.com>
2023-02-11 13:08:17 +0300

    change logic of game

--- src/games/even_game.js
+++ src/games/even_game.js
@@ -1,44 +1,23 @@
 import _ from "lodash";
 import readlineSync from "readline-sync";
+import getGamePlay from "../index.js";
 
-const ruleOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
+export const ruleOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}`);
-console.log(ruleOfGame);
+const isEven = (randomNumber) => randomNumber % 2 === 0;
 
 const brainEven = () => {
-  for (let i = 0; i < 3; i += 1) {
-    let randomNumber = _.random(1, 100);
+  let randomNumber = _.random(1, 100);
+  
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
 
-    console.log(`Question: ${randomNumber}`);
-
-    const userAnswer = readlineSync.question("Your answer: ");
-
-    if (randomNumber % 2 === 0 && userAnswer === "yes") {
-      console.log("Correct!");
-    } else if (randomNumber % 2 !== 0 && userAnswer === "no") {
-      console.log("Correct!");
-    } else if (randomNumber % 2 === 0 && userAnswer === "no") {
-      console.log(
-        `${userAnswer} is wrong answer ;(. Correct answer was 'yes'.`
-      );
-      console.log(`Let's try again, ${userName}!`);
-      break;
-    } else if (randomNumber % 2 !== 0 && userAnswer === "yes") {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.`);
-      console.log(`Let's try again, ${userName}!`);
-      break;
-    } else {
-      console.log(`Let's try again, ${userName}!`);
-      break;
-    }
-
-    if (i === 2) {
-      console.log(`Congratulations, ${userName}!`);
-    }
-  }
+  return [randomNumber, correctAnswer];
 };
 
-export default brainEven;
+const brainEvenGame = () => {
+  return getGamePlay(ruleOfGame, brainEven());
+};
+
+export default brainEvenGame;
+
+console.log(brainEvenGame());
