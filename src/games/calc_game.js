3aacee5901442b185d61bbf0ac736b33b4c88df9
Svetlana <pidgin66@gmail.com>
2023-02-11 21:29:22 +0300

    add calc game

---
+++ src/games/calc_game.js
@@ -0,0 +1,33 @@
+import _ from "lodash";
+import getGamePlay from "../index.js";
+
+export const ruleOfGame = 'What is the result of the expression?';
+
+const operators = ['+', '-', '*'];
+
+const brainCalc = () => {
+    const randomOperator = _.random(0, 2);
+
+const firstOperand = _.random(1, 10);
+
+const secondOperand = _.random(1, 10);
+    const expression = `${firstOperand} ${operators[randomOperator]} ${secondOperand}`;
+    let result = 0;
+    if (operators[randomOperator] === '+') {
+        result = firstOperand + secondOperand;
+    } else if (operators[randomOperator] === '-') {
+        result = firstOperand - secondOperand;
+    } else if (operators[randomOperator] === '*') {
+        result = firstOperand * secondOperand;
+    };
+
+    let answer = result.toString();
+
+    return [expression, answer];
+};
+
+const brainCalcGame = () => {
+    return getGamePlay(ruleOfGame, brainCalc);
+};
+
+export default brainCalcGame;
