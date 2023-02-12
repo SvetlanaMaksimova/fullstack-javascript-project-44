d11ba14b31e3a4f14c17f7c8ba9603e78f71e375
Svetlana <pidgin66@gmail.com>
2023-02-12 19:31:56 +0300

    add gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import brainGcdGame from '../src/games/gcd_game.js';
+
+brainGcdGame();
---
+++ src/games/gcd_game.js
@@ -0,0 +1,33 @@
+import _ from "lodash";
+import getGamePlay from "../index.js";
+
+export const ruleOfGame = "Find the greatest common divisor of given numbers.";
+
+const brainGcd = () => {
+  const firstRandomNumber = _.random(1, 100);
+  const secondRandomNumber = _.random(1, 100);
+  let firstNumber = firstRandomNumber;
+  let secondNumber = secondRandomNumber;
+  let result = 0;
+  while (firstNumber !== secondNumber) {
+    if (firstNumber < secondNumber) {
+      result = firstNumber;
+      firstNumber = secondNumber - firstNumber;
+      secondNumber = result;
+    } else {
+      result = secondNumber;
+      secondNumber = firstNumber - secondNumber;
+      firstNumber = result;
+    }
+  }
+  const numbers = `${firstRandomNumber} ${secondRandomNumber}`;
+  const answer = result.toString();
+
+  return [numbers, answer];
+};
+
+const brainGcdGame = () => {
+  return getGamePlay(ruleOfGame, brainGcd);
+};
+
+export default brainGcdGame;
