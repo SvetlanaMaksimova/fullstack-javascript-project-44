2bf82bd153994e3a91809bc14de3944b2bb09d6e
Svetlana <pidgin66@gmail.com>
2023-02-12 21:40:17 +0300

    add progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import brainProgressiveGame from '../src/games/progressive_game.js';
+
+brainProgressiveGame();
---
+++ src/games/progressive_game.js
@@ -0,0 +1,29 @@
+import _ from "lodash";
+import getGamePlay from "../index.js";
+
+const ruleOfGame = "What number is missing in the progression?";
+
+const brainProgressive = () => {
+  let randomStartArr = _.random(1, 50);
+  const randomStep = _.random(1, 10);
+  const randomHide = _.random(0, 9);
+  const progressive = [];
+
+  while (progressive.length < 10) {
+    progressive.push(randomStartArr);
+    randomStartArr += randomStep;
+  }
+  const answer = progressive[randomHide].toString();
+
+  progressive[randomHide] = "..";
+
+  const question = progressive.toString();
+
+  return [question, answer];
+};
+
+const brainProgressiveGame = () => {
+  return getGamePlay(ruleOfGame, brainProgressive);
+};
+
+export default brainProgressiveGame;
