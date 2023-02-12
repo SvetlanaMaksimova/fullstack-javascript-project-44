6191943d300bce44087a532681ef7de09da80713
Svetlana <pidgin66@gmail.com>
2023-02-12 19:32:46 +0300

    formatting

--- src/games/calc_game.js
+++ src/games/calc_game.js
@@ -1,33 +1,33 @@
 import _ from "lodash";
 import getGamePlay from "../index.js";
 
-export const ruleOfGame = 'What is the result of the expression?';
+export const ruleOfGame = "What is the result of the expression?";
 
-const operators = ['+', '-', '*'];
+const operators = ["+", "-", "*"];
 
 const brainCalc = () => {
-    const randomOperator = _.random(0, 2);
+  const randomOperator = _.random(0, 2);
 
-const firstOperand = _.random(1, 10);
+  const firstOperand = _.random(1, 10);
 
-const secondOperand = _.random(1, 10);
-    const expression = `${firstOperand} ${operators[randomOperator]} ${secondOperand}`;
-    let result = 0;
-    if (operators[randomOperator] === '+') {
-        result = firstOperand + secondOperand;
-    } else if (operators[randomOperator] === '-') {
-        result = firstOperand - secondOperand;
-    } else if (operators[randomOperator] === '*') {
-        result = firstOperand * secondOperand;
-    };
+  const secondOperand = _.random(1, 10);
+  const expression = `${firstOperand} ${operators[randomOperator]} ${secondOperand}`;
+  let result = 0;
+  if (operators[randomOperator] === "+") {
+    result = firstOperand + secondOperand;
+  } else if (operators[randomOperator] === "-") {
+    result = firstOperand - secondOperand;
+  } else if (operators[randomOperator] === "*") {
+    result = firstOperand * secondOperand;
+  }
 
-    let answer = result.toString();
+  let answer = result.toString();
 
-    return [expression, answer];
+  return [expression, answer];
 };
 
 const brainCalcGame = () => {
-    return getGamePlay(ruleOfGame, brainCalc);
+  return getGamePlay(ruleOfGame, brainCalc);
 };
 
 export default brainCalcGame;
