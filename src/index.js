01d402c525e91859e952473e317261a1f35927de
Svetlana <pidgin66@gmail.com>
2023-02-16 13:35:32 +0300

    fix local const to global

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import readlineSync from 'readline-sync';
 
+const countRounds = 3;
+
 const getGamePlay = (ruleOfGame, valuesOfGame) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
   console.log(ruleOfGame);
-
-  const countRounds = 3;
 
   for (let i = 0; i < countRounds; i += 1) {
     const [question, answer] = valuesOfGame();
