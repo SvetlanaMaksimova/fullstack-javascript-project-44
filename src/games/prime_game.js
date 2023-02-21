fa1a25a6a30be908df0af5be248b5f412646d39d
Svetlana <pidgin66@gmail.com>
2023-02-21 20:32:31 +0300

    moved func isPrime from call func

--- src/games/prime_game.js
+++ src/games/prime_game.js
@@ -5,19 +5,19 @@ const ruleOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no"
 
 const isPrime = (number) => {
   if (number < 2) {
-    return 'no';
+    return false;
   }
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
 
 const brainPrime = () => {
   const number = _.random(2, 50);
-  const answer = isPrime(number);
+  const answer = isPrime(number) ? 'yes' : 'no';
   return [number, answer];
 };
 
