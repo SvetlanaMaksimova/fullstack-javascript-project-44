bd5598375e9ddfc102079bfa78a85d9135e4a835
Svetlana <pidgin66@gmail.com>
2023-02-21 20:19:21 +0300

    moved function calculation

--- src/games/calc_game.js
+++ src/games/calc_game.js
@@ -5,23 +5,25 @@ const ruleOfGame = 'What is the result of the expression?';
 
 const operators = ['+', '-', '*'];
 
+const calculation = (num1, num2, oper) => {
+  switch (operator) {
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    case '*': return num1 * num2;
+    default: throw new Error(`Operator ${oper} - is invalid!`)
+  } 
+}
+
 const brainCalc = () => {
   const randomOperator = _.random(0, 2);
 
   const firstOperand = _.random(1, 10);
 
   const secondOperand = _.random(1, 10);
+  
   const expression = `${firstOperand} ${operators[randomOperator]} ${secondOperand}`;
-  let result = 0;
-  if (operators[randomOperator] === '+') {
-    result = firstOperand + secondOperand;
-  } else if (operators[randomOperator] === '-') {
-    result = firstOperand - secondOperand;
-  } else if (operators[randomOperator] === '*') {
-    result = firstOperand * secondOperand;
-  }
 
-  const answer = result.toString();
+  const answer = calculation(firstOperand, secondOperand,operators[randomOperator]).toString();
 
   return [expression, answer];
 };
